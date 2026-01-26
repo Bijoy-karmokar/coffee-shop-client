e73a1857e9c1afc8c3b2022758fddbab0449cd08
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-01-26 18:55:49 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from "../assets/more/logo1.png"
 
 const Navbar = () => {
     return (
-        <div>
-           <img src="" alt="" />
+        <div className='flex items-center justify-center p-1 text-white bg-orange-950'>
+           <img className='w-20' src={logo} alt="" />
            <p className='text-5xl md:text-6xl'>Espresso Emporium</p>
         </div>
     );
