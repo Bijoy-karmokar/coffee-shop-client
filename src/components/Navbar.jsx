c63a324bddeda57476eaab7af3057f96070b50a9
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-01-26 18:46:43 +0600

    image and icons add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            Navbar
+           <img src="" alt="" />
+           <p className='text-5xl md:text-6xl'>Espresso Emporium</p>
         </div>
     );
 };
